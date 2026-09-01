4e925f9474b7af7361af78bdea1696a36482af2c
Subramanya <Mrsubramanya7@gmail.com>
2026-09-01 16:56:54 +0530

    Validation & Loaded data into premium_raw

--- snowflake/05_raw_ingestion.sql
+++ snowflake/05_raw_ingestion.sql
@@ -25,3 +25,24 @@ COPY INTO CUSTOMER_RAW
 FROM @CUSTOMER_S3_STAGE
 FILE_FORMAT = (FORMAT_NAME = 'CUSTOMER_CSV_FORMAT')
 ON_ERROR = 'ABORT_STATEMENT';
+
+
+
+-- =========================================================
+-- 1.PREMIUM DATA VALIDATION
+-- =========================================================
+
+COPY INTO PREMIUM_RAW
+FROM @PREMIUM_S3_STAGE
+FILE_FORMAT = (FORMAT_NAME = 'PREMIUM_CSV_FORMAT')
+VALIDATION_MODE = 'RETURN_ERRORS';
+
+
+-- =========================================================
+-- 2.PREMIUM DATA LOAD
+-- =========================================================
+
+COPY INTO PREMIUM_RAW
+FROM @PREMIUM_S3_STAGE
+FILE_FORMAT = (FORMAT_NAME = 'PREMIUM_CSV_FORMAT')
+ON_ERROR = 'ABORT_STATEMENT';
