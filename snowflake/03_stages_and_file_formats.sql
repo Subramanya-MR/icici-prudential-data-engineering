3585d84824b768f97f47f21c432a6a7d809f4cc7
Subramanya <Mrsubramanya7@gmail.com>
2026-09-01 16:49:24 +0530

    ADD stage and file_format for premium

--- snowflake/03_stages_and_file_formats.sql
+++ snowflake/03_stages_and_file_formats.sql
@@ -46,3 +46,25 @@ CREATE OR REPLACE FILE FORMAT POLICY_CSV_FORMAT
     SKIP_HEADER = 1
     FIELD_OPTIONALLY_ENCLOSED_BY = '"'
     TRIM_SPACE = TRUE;
+
+
+
+
+-- ---------------------------------------------------------
+-- PREMIUM S3 STAGE
+-- ---------------------------------------------------------
+
+CREATE OR REPLACE STAGE PREMIUM_S3_STAGE
+    URL = 's3://icici-prudential-data/premium/2026/08/26/'
+    STORAGE_INTEGRATION = ICICI_S3_INTEGRATION;
+
+
+-- ---------------------------------------------------------
+-- PREMIUM CSV FILE FORMAT
+-- ---------------------------------------------------------
+
+CREATE OR REPLACE FILE FORMAT PREMIUM_CSV_FORMAT
+    TYPE = CSV
+    SKIP_HEADER = 1
+    FIELD_OPTIONALLY_ENCLOSED_BY = '"'
+    TRIM_SPACE = TRUE;
