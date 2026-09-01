f9999a1d58e0b5aa104f42b36b5ca7739a1df544
Subramanya <Mrsubramanya7@gmail.com>
2026-09-01 16:17:44 +0530

    Add policy stage and CSV file format

--- snowflake/03_stages_and_file_formats.sql
+++ snowflake/03_stages_and_file_formats.sql
@@ -25,3 +25,24 @@ CREATE OR REPLACE FILE FORMAT CUSTOMER_CSV_FORMAT
     SKIP_HEADER = 1
     FIELD_OPTIONALLY_ENCLOSED_BY = '"'
     TRIM_SPACE = TRUE;
+
+
+
+-- ---------------------------------------------------------
+-- POLICY S3 STAGE
+-- ---------------------------------------------------------
+
+CREATE OR REPLACE STAGE POLICY_S3_STAGE
+    URL = 's3://icici-prudential-data/ policy/2026/08/26/'
+    STORAGE_INTEGRATION = ICICI_S3_INTEGRATION;
+
+
+-- ---------------------------------------------------------
+-- POLICY CSV FILE FORMAT
+-- ---------------------------------------------------------
+
+CREATE OR REPLACE FILE FORMAT POLICY_CSV_FORMAT
+    TYPE = CSV
+    SKIP_HEADER = 1
+    FIELD_OPTIONALLY_ENCLOSED_BY = '"'
+    TRIM_SPACE = TRUE;
